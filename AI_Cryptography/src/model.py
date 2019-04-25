756a8e3c9afe43a1c7910f476c1e279b0d70857c
takhyun12 <41291493+takhyun12@users.noreply.github.com>
2019-04-25 11:23:59 +0900

    Add files via upload

---
+++ AI_Cryptography/src/model.py
@@ -0,0 +1,137 @@
+import tensorflow as tf
+import numpy as np
+
+import matplotlib
+# OSX fix
+matplotlib.use('TkAgg')
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from .layers import conv_layer
+from .config import *
+from .utils import init_weights, gen_data
+
+
+class CryptoNet(object):
+    def __init__(self, sess, msg_len=MSG_LEN, batch_size=BATCH_SIZE,
+                 epochs=NUM_EPOCHS, learning_rate=LEARNING_RATE):
+        """
+        Args:
+            sess: TensorFlow 세션
+            msg_len: 암호화할 입력 메시지의 길이.
+            key_len: Alice와 Bob의 개인 키의 길이.
+            batch_size: 각 적대적 교육에 대한 최소 배치 크기
+            epochs: 적군 훈련의 수
+            learning_rate: Adam Optimizer의 학습 속도
+        """
+
+        self.sess = sess
+        self.msg_len = msg_len
+        self.key_len = self.msg_len
+        self.N = self.msg_len
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.learning_rate = learning_rate
+
+        self.build_model()
+
+    def build_model(self):
+        # Weights for fully connected layers
+        self.w_alice = init_weights("alice_w", [2 * self.N, 2 * self.N])
+        self.w_bob = init_weights("bob_w", [2 * self.N, 2 * self.N])
+        self.w_eve1 = init_weights("eve_w1", [self.N, 2 * self.N])
+        self.w_eve2 = init_weights("eve_w2", [2 * self.N, 2 * self.N])
+
+        # Placeholder variables for Message and Key
+        self.msg = tf.placeholder("float", [None, self.msg_len])
+        self.key = tf.placeholder("float", [None, self.key_len])
+
+        # Alice's network
+        self.alice_input = tf.concat([self.msg, self.key],1)
+        self.alice_hidden = tf.nn.sigmoid(tf.matmul(self.alice_input, self.w_alice))
+        self.alice_hidden = tf.expand_dims(self.alice_hidden, 2)
+        self.alice_output = tf.squeeze(conv_layer(self.alice_hidden, "alice"))
+
+        # Bob's network
+        self.bob_input = tf.concat([self.alice_output, self.key],1)
+        self.bob_hidden = tf.nn.sigmoid(tf.matmul(self.bob_input, self.w_bob))
+        self.bob_hidden = tf.expand_dims(self.bob_hidden, 2)
+        self.bob_output = tf.squeeze(conv_layer(self.bob_hidden, "bob"))
+
+        # Eve's network
+        self.eve_input = self.alice_output
+        self.eve_hidden1 = tf.nn.sigmoid(tf.matmul(self.eve_input, self.w_eve1))
+        self.eve_hidden2 = tf.nn.sigmoid(tf.matmul(self.eve_hidden1, self.w_eve2))
+        self.eve_hidden2 = tf.expand_dims(self.eve_hidden2, 2)
+        self.eve_output = tf.squeeze(conv_layer(self.eve_hidden2, "eve"))
+
+    def train(self):
+        # Loss Functions
+        self.decrypt_err_eve = tf.reduce_mean(tf.abs(self.msg - self.eve_output))
+        self.decrypt_err_bob = tf.reduce_mean(tf.abs(self.msg - self.bob_output))
+        self.loss_bob = self.decrypt_err_bob + (1. - self.decrypt_err_eve) ** 2.
+
+        # Get training variables corresponding to each network
+        self.t_vars = tf.trainable_variables()
+        self.alice_or_bob_vars = [var for var in self.t_vars if 'alice_' in var.name or 'bob_' in var.name]
+        self.eve_vars = [var for var in self.t_vars if 'eve_' in var.name]
+
+        # Build the optimizers
+        self.bob_optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(
+            self.loss_bob, var_list=self.alice_or_bob_vars)
+        self.eve_optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(
+            self.decrypt_err_eve, var_list=self.eve_vars)
+
+        self.bob_errors, self.eve_errors = [], []
+
+        # Begin Training
+        tf.global_variables_initializer().run()
+        for i in range(self.epochs):
+            iterations = 2000
+
+            print('Training Alice and Bob, Epoch:', i + 1)
+            bob_loss, _ = self._train('bob', iterations)
+            self.bob_errors.append(bob_loss)
+
+            print('Training Eve, Epoch:', i + 1)
+            _, eve_loss = self._train('eve', iterations)
+            self.eve_errors.append(eve_loss)
+
+        self.plot_errors()
+
+    def _train(self, network, iterations):
+        bob_decrypt_error, eve_decrypt_error = 1., 1.
+
+        bs = self.batch_size
+        # Train Eve for two minibatches to give it a slight computational edge
+        if network == 'eve':
+            bs *= 2
+
+        for i in range(iterations):
+            msg_in_val, key_val = gen_data(n=bs, msg_len=self.msg_len, key_len=self.key_len)
+            print(msg_in_val)
+            if network == 'bob':
+                _, decrypt_err = self.sess.run([self.bob_optimizer, self.decrypt_err_bob],
+                                               feed_dict={self.msg: msg_in_val, self.key: key_val})
+                bob_decrypt_error = min(bob_decrypt_error, decrypt_err)
+
+            elif network == 'eve':
+                _, decrypt_err = self.sess.run([self.eve_optimizer, self.decrypt_err_eve],
+                                               feed_dict={self.msg: msg_in_val, self.key: key_val})
+                eve_decrypt_error = min(eve_decrypt_error, decrypt_err)
+
+        return bob_decrypt_error, eve_decrypt_error
+
+    def plot_errors(self):
+        """
+        Plot Lowest Decryption Errors achieved by Bob and Eve per epoch
+        """
+        sns.set_style("darkgrid")
+        plt.plot(self.bob_errors)
+        plt.plot(self.eve_errors)
+        plt.legend(['bob', 'eve'])
+        plt.xlabel('Epoch')
+        plt.ylabel('Lowest Decryption error achieved')
+        plt.show()
+
